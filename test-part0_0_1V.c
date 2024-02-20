fa50224626c4fa364154c26e2a1f285786c605a6
Suwiwat Sinsomboon <159708334+iceswift@users.noreply.github.com>
2024-02-20 07:19:21 +0000

    Added Features

--- test-part0_0_1V.c
+++ test-part0_0_1V.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
+int main() {
+    int dividend, divisor, quotient, remainder;
+    printf("Enter dividend: ");
+    scanf("%d", &dividend);
+    printf("Enter divisor: ");
+    scanf("%d", &divisor);
 
-int main(){
-    int hit;
-    scanf("%d", hit);
+    // Computes quotient
+    quotient = dividend / divisor;
 
-    printf("%d", hit+20);
+    // Computes remainder
+    remainder = dividend % divisor;
+
+    printf("Quotient = %d\n", quotient);
+    printf("Remainder = %d", remainder);
+    return 0;
 }
